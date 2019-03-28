63900a2406a9548e05faf6580a7c0d430c39783d
Ike Njoku <ikeenjoku@gmail.com>
2019-03-28 20:22:27 +0100

    Setup root reducer and store

---
+++ redux/store.js
@@ -0,0 +1,14 @@
+import { createStore } from "redux";
+import initialState from './initialState';
+
+
+const rootReducer = combineReducers({
+  leaderboard : leaderboardReducer,
+  question : questionReducer,
+  modals : modalsReducer,
+  playerInfo : playerInfoReducer,
+  gameData : gameDataReducer
+});
+
+
+export default createStore(rootReducer, initialState);
